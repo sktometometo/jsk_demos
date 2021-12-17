ac4f4c5e38776f7545c48f289d47bb5b7f690ca1
Koki Shinjo <sktometometo@gmail.com>
2021-12-17 14:55:33 +0900

    [jsk_maps] fix format

--- jsk_maps/tools/static_lon_lat_transform_publisher.py
+++ jsk_maps/tools/static_lon_lat_transform_publisher.py
@@ -5,9 +5,9 @@ import rospy
 import tf2_ros
 from geometry_msgs.msg import TransformStamped
 import math
-import staticmap
 
 import jsk_maps
+
 
 class StaticLonLatPublisher:
 
@@ -21,15 +21,16 @@ class StaticLonLatPublisher:
         self.to_latitude = rospy.get_param('~to_latitude')
         self.to_longitude = rospy.get_param('~to_longitude')
 
-        self.diff_angle_from_to = rospy.get_param('~diff_angle_from_to') # radians
+        self.diff_angle_from_to = rospy.get_param(
+            '~diff_angle_from_to')  # radians
 
         self.tf_br = tf2_ros.StaticTransformBroadcaster()
 
         diff_x, diff_y = jsk_maps.calc_transform_from_lon_lat(
-                            self.from_longitude,
-                            self.from_latitude,
-                            self.to_longitude,
-                            self.to_latitude )
+            self.from_longitude,
+            self.from_latitude,
+            self.to_longitude,
+            self.to_latitude)
 
         transform = TransformStamped()
         transform.header.frame_id = self.from_frame_id
@@ -46,4 +47,3 @@ if __name__ == '__main__':
     rospy.init_node('static_lon_lat_publisher', anonymous=True)
     node = StaticLonLatPublisher()
     rospy.spin()
-
