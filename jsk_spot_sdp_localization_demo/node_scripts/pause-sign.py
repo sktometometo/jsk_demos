78868017664ce93e63fb4a679887763623ecf4a3
Koki Shinjo <sktometometo@gmail.com>
2024-04-08 13:41:09 +0900

    Update

--- jsk_spot_sdp_localization_demo/node_scripts/pause-sign.py
+++ jsk_spot_sdp_localization_demo/node_scripts/pause-sign.py
@@ -20,17 +20,29 @@ class PauseSignExecutor:
 
         self._interface = UWBSDPInterface()
         self._client = SpotROSClient()
+        self._sdp_interface = UWBSDPInterface()
         self._lock_mobility = ROSLock("mobility")
 
-        self._sub_odom = rospy.Subscriber("/spot/odometry", Odometry, self._cb_odom)
         self._odom_to_base: Optional[PyKDL.Frame] = None
         self._lock_odom = threading.Lock()
 
+        self._nearest_pause_sign_dist: Optional[float] = None
+        self._lock_nearest_pause_sign_dist = threading.Lock()
+
+        self._sub_odom = rospy.Subscriber("/spot/odometry", Odometry, self._cb_odom)
         self._sub_sdpuwb = rospy.Subscriber(
             "/sdpuwb_devices", SDPUWBDeviceArray, self._cb_device
         )
 
-        self._sdp_interface = UWBSDPInterface()
+    @property
+    def odom_to_base(self):
+        with self._lock_odom:
+            return copy.deepcopy(self._odom_to_base)
+
+    @property
+    def nearest_pause_sign_dist(self):
+        with self._lock_nearest_pause_sign_dist
+            return self._nearest_pause_sign_dist
 
     def _cb_odom(self, msg: Odometry):
         with self._lock_odom:
@@ -49,18 +61,66 @@ class PauseSignExecutor:
             )
 
     def _cb_device(self, msg: SDPUWBDeviceArray):
+        if len(msg.devices) == 0:
+            rospy.logwarn('lengh of meessag is zero. skpped')
+            return
+
         with self._lock_odom:
-            if self._odom_to_base is None:
+            if self.odom_to_base is None:
                 return
+
         dev_ifs = copy.deepcopy(self._sdp_interface.device_interfaces)
-        for dev in msg.devices:
-            if dev.device_name not in [v["device_name"] for v in dev_ifs.values()]:
-                continue
-            frame_odom_to_device = PyKDL.Frame(
-                PyKDL.Rotation(),
-                PyKDL.Vector(
-                    dev.point.x,
-                    dev.point.y,
-                    dev.point.z,
-                ),
-            )
+
+        valid_devices = [
+                    msg_dev
+                    for msg_dev in msg.devices
+                    if msg_dev.device_name in dev_ifs
+                    and dev_ifs[msg_dev.device_name]["distance"] is not None
+                    and 'PauseSign' in msg_dev.device_name
+                ]
+        if len(valid_devices) == 0:
+            rospy.logwarn('lengh of valid devices is zero. skpped')
+            return
+
+        target_device = sorted(
+                valid_devices,
+                key=lambda x: dev_ifs[x.device_name]["distance"]
+                )[0]
+
+        with self._lock_nearest_pause_sign_dist:
+            self._nearest_pause_sign_dist = dev_ifs[target_device.device_name]["distance"]
+
+
+    def run(self):
+
+        rospy.loginfo("First, move the robot to the 2F elevator hall of eng2.")
+        input("Press Enter > ")
+        self._client.upload_graph("/home/spot/default.walk")
+        self._client.set_localization_waypoint('daft-fleece-OI80B3zjIRf0G4nxz5YLwA==')
+        self._client.set_localization_fiducial()
+
+        self._client.gripper_open()
+        input("Give me document. Press Enter > ")
+        self._client.gripper_close()
+
+        rospy.loginfo("Move to Reppincan")
+        self._client.navigate_to("staple-finch-GrUHQoxOBscHvFykmFcrkQ==", blocking=True)
+
+        input("Give me document. Press Enter > ")
+        self._client.gripper_open()
+
+        self._client.navigate_to("daft-fleece-OI80B3zjIRf0G4nxz5YLwA==", blocking=False)
+        while not rospy.is_shutdown():
+            if self._client.wait_for_navigate_to_result(duration=rospy.Duration(1)):
+                return
+            if self._nearest_pause_sign_dist < 5.0:
+                self._client.cancel_navigate_to()
+                break
+
+        self._client.navigate_to("alike-tapir-5izZBNcxp+fG+UmYKLniNQ==", blocking=True)
+
+
+if __name__ == '__main__':
+    rospy.init_node('hoge')
+    node = PauseSignExecutor()
+    node.run()
