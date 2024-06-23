5b4e43662b6f16b960389dc764d6cebb1ce73e7f
Koki Shinjo <sktometometo@gmail.com>
2024-06-23 20:31:46 +0900

    udpate

--- jsk_spot_sdp_localization_demo/node_scripts/stickv2_face_recognition_demo_visualizer.py
+++ jsk_spot_sdp_localization_demo/node_scripts/stickv2_face_recognition_demo_visualizer.py
@@ -6,7 +6,10 @@ from dataclasses import dataclass
 from typing import Dict, List, Optional
 
 import rospy
+import yaml
+from std_msgs.msg import String
 from visualization_msgs.msg import Marker, MarkerArray
+from yaml.constructor import ConstructorError
 
 
 @dataclass
@@ -19,6 +22,31 @@ class FaceRecognitionInfo:
         return True
 
 
+class SafeLoaderWithTuple(yaml.SafeLoader):
+    pass
+
+
+def construct_tuple(loader, node):
+    try:
+        return tuple(loader.construct_sequence(node))
+    except ConstructorError as e:
+        raise ConstructorError(
+            "could not determine a constructor for the tag 'tag:yaml.org,2002:python/tuple'",
+            e.problem_mark,
+        )
+
+
+def construct_table_entry(loader, node):
+    node = loader.construct_mapping(node)
+    return node
+
+
+SafeLoaderWithTuple.add_constructor("tag:yaml.org,2002:python/tuple", construct_tuple)
+SafeLoaderWithTuple.add_constructor(
+    "tag:yaml.org,2002:python/object:__main__.TableEntry", construct_table_entry
+)
+
+
 class FaceRecognitionVisualizer:
 
     def __init__(self):
@@ -29,25 +57,42 @@ class FaceRecognitionVisualizer:
             queue_size=1,
         )
 
-        self.device_dict: Dict[str, FaceRecognitionInfo] = dict()
+        self.device_dict: Dict[str, FaceRecognitionInfo] = {}
         self.lock_device_dict = threading.Lock()
+
+        self._sub_debug = rospy.Subscriber(
+            "/debug_string",
+            String,
+            self._cb_debug,
+        )
+
+    def _cb_debug(self, msg: String):
+        data = yaml.load(msg.data, Loader=SafeLoaderWithTuple)
+        print(data)
+        if "device_neareset_node" in data:
+            with self.lock_device_dict:
+                self.device_dict[data["device_neareset_node"]["nearest_node_id"]] = (
+                    FaceRecognitionInfo(
+                        device_name=data["device_neareset_node"]["device_name"],
+                        waypoint_id=data["device_neareset_node"]["nearest_node_id"],
+                        detected=True,
+                    )
+                )
 
     def _publish_marker(self):
 
         marker_array = MarkerArray()
         with self.lock_device_dict:
+            print(self.device_dict)
             for waypoint_id, device_info in self.device_dict.items():
-                if not device_info.is_complete():
-                    show = False
-                else:
-                    show = True
                 #
                 marker = Marker()
-                marker.header = device_info.waypoint_id
+                marker.header.stamp = rospy.Time.now()
+                marker.header.frame_id = "waypoint_" + device_info.waypoint_id
                 marker.ns = "light_room_device"
                 marker.id = hash(waypoint_id) % 214748364
                 marker.type = Marker.CYLINDER
-                marker.action = Marker.ADD if show else Marker.DELETE
+                marker.action = Marker.ADD
                 marker.pose.position.x = 0
                 marker.pose.position.y = 0
                 marker.pose.position.z = 0.75
@@ -67,11 +112,12 @@ class FaceRecognitionVisualizer:
                 marker_array.markers.append(marker)
                 #
                 marker = Marker()
-                marker.header = device_info.waypoint_id
+                marker.header.stamp = rospy.Time.now()
+                marker.header.frame_id = "waypoint_" + device_info.waypoint_id
                 marker.ns = "light_room_device"
                 marker.id = hash(waypoint_id) % 214748364 + 1
                 marker.type = Marker.CYLINDER
-                marker.action = Marker.ADD if show else Marker.DELETE
+                marker.action = Marker.ADD
                 marker.pose.position.x = 0
                 marker.pose.position.y = 0
                 marker.pose.position.z = 1.5
--- jsk_spot_sdp_localization_demo/scripts/rosbag_play_fd_demo.sh
+++ jsk_spot_sdp_localization_demo/scripts/rosbag_play_fd_demo.sh
@@ -6,11 +6,59 @@ function abs_path() {
     echo "$(cd "$(dirname "$1")" && pwd)/$(basename "$1")"
 }
 
-ROSBAG_FILES=($@)
+function usage() {
+    echo "Usage: $0 <rosbag_file1> <rosbag_file2> ... [-s START_TIME]"
+    exit 1
+}
+
+START_TIME=""
+
+while getopts ":s:" opt; do
+    case ${opt} in
+    s)
+        START_TIME=$OPTARG
+        ;;
+    \?)
+        echo "Invalid option: -$OPTARG" >&2
+        usage
+        ;;
+    :)
+        echo "Option -$OPTARG requires an argument." >&2
+        usage
+        ;;
+    esac
+done
+shift $((OPTIND - 1))
+
+# Check if at least one rosbag file is provided
+if [ $# -lt 1 ]; then
+    usage
+fi
+
+ROSBAG_FILES=()
+while [[ $# -gt 0 ]]; do
+    case $1 in
+    -s)
+        if [ -n "$2" ]; then
+            START_TIME=$2
+            shift 2
+        else
+            echo "Option -s requires an argument." >&2
+            usage
+        fi
+        ;;
+    *)
+        ROSBAG_FILES+=("$1")
+        shift
+        ;;
+    esac
+done
+
+echo "ROSBAG_FILES: ${ROSBAG_FILES[@]}"
+echo "START_TIME: ${START_TIME}"
 
 if [ ${#ROSBAG_FILES[@]} -eq 0 ]; then
-    echo "Usage: $0 <rosbag_file1> <rosbag_file2> ..."
-    exit 1
+    usage
 fi
 
 ABSPATH_ROSBAG_FILES=""
@@ -22,4 +70,4 @@ done
 echo "ROSBAG_FILES: ${ROSBAG_FILES[@]}"
 echo "ABSPATH_ROSBAG_FILES: ${ABSPATH_ROSBAG_FILES}"
 
-roslaunch jsk_spot_sdp_localization_demo rosbag_play_fd_demo.launch rosbag:="${ABSPATH_ROSBAG_FILES}"
+roslaunch jsk_spot_sdp_localization_demo rosbag_play_fd_demo.launch rosbag:="${ABSPATH_ROSBAG_FILES}" start_time:=${START_TIME}
