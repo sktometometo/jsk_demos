240e5a91221bfd49446daa3ac51ad8bfa1e8c74a
Koki Shinjo <sktometometo@gmail.com>
2024-08-25 16:41:11 +0900

    update

---
+++ jsk_spot_autonomous_integration_demo/node_scripts/demo.py
@@ -0,0 +1,323 @@
+#!/usr/bin/env python3
+
+import copy
+import math
+import threading
+import time
+from operator import is_
+from typing import Dict, List, Optional
+
+import PyKDL
+import rospy
+import yaml
+from jsk_recognition_msgs.msg import BoundingBoxArray
+from jsk_spot_lib.look_at_client import SpotLookAtClient
+from nav_msgs.msg import Odometry
+from ros_lock import ROSLock, roslock_acquire
+from smart_device_protocol.smart_device_protocol_interface import (
+    DataFrame, UWBSDPInterface)
+from sound_play.libsoundplay import SoundClient
+from spot_ros_client.libspotros import SpotRosClient
+from std_msgs.msg import String
+from std_srvs.srv import Trigger, TriggerRequest
+from uwb_localization.msg import SDPUWBDevice, SDPUWBDeviceArray
+
+
+class LightRoomDemo:
+
+    def __init__(self):
+
+        self._interface = UWBSDPInterface()
+        self._client = SpotRosClient()
+        self._sound_client = SoundClient()
+        self._sdp_interface = UWBSDPInterface()
+
+        self._reset_localize = rospy.ServiceProxy("/device_localization_node/reset", Trigger)
+
+        self._sdp_interface.register_interface_callback(
+            ("Light status", "?"),
+            self._cb_sdp,
+        )
+
+        self._vel_limit = (0.5, 0.5, 0.3)
+
+        self._turn_on_distance_threshold: float = 3.0
+
+        self._odom_to_base: Optional[PyKDL.Frame] = None
+        self._lock_odom = threading.Lock()
+
+        self._odom_to_people: List[PyKDL.Frame] = []
+        self._lock_people = threading.Lock()
+
+        self._pub_demo_debug = rospy.Publisher('~debug', String, queue_size=1)
+
+        self._light_status_table_lock = threading.Lock()
+        self._light_status_table: Dict[str, bool] = {}
+
+        self._sub_odom = rospy.Subscriber("/spot/odometry", Odometry, self._cb_odom)
+        self._sub_people_bbox = rospy.Subscriber(
+            "/spot_recognition/bbox_array",
+            BoundingBoxArray,
+            self._cb_people_bbox,
+        )
+
+        self._running_demo_thread = True
+        self._thread_demo = threading.Thread(target=self.demo)
+        self._thread_demo.start()
+
+        print("initialized")
+
+    @property
+    def device_interfaces(self):
+        return copy.deepcopy(self._interface.device_interfaces)
+
+    @property
+    def odom_to_base(self):
+        with self._lock_odom:
+            return copy.deepcopy(self._odom_to_base)
+
+    @property
+    def odom_to_people(self):
+        with self._lock_people:
+            return copy.deepcopy(self._odom_to_people)
+
+    def _cb_odom(self, msg: Odometry):
+        with self._lock_odom:
+            self._odom_to_base = PyKDL.Frame(
+                PyKDL.Rotation.Quaternion(
+                    msg.pose.pose.orientation.x,
+                    msg.pose.pose.orientation.y,
+                    msg.pose.pose.orientation.z,
+                    msg.pose.pose.orientation.w,
+                ),
+                PyKDL.Vector(
+                    msg.pose.pose.position.x,
+                    msg.pose.pose.position.y,
+                    msg.pose.pose.position.z,
+                ),
+            )
+
+    def _cb_sdp(self, src_address, data_frame: DataFrame):
+        device_content = data_frame.content[0]
+        device_interfaces = self.device_interfaces
+        if src_address not in device_interfaces:
+            rospy.logerr(f"Failed to find device interface for {src_address}")
+            return
+        device_name = device_interfaces[src_address]["device_name"]
+        with self._light_status_table_lock:
+            self._light_status_table[device_name] = device_content
+
+    def _cb_people_bbox(self, msg: BoundingBoxArray):
+        with self._lock_people:
+            self._odom_to_people = [
+                PyKDL.Frame(
+                    PyKDL.Rotation.Quaternion(
+                        msg_box.pose.orientation.x,
+                        msg_box.pose.orientation.y,
+                        msg_box.pose.orientation.z,
+                        msg_box.pose.orientation.w,
+                    ),
+                    PyKDL.Vector(
+                        msg_box.pose.position.x,
+                        msg_box.pose.position.y,
+                        msg_box.pose.position.z,
+                    ),
+                )
+                for msg_box in msg.boxes
+            ]
+
+    def turn_light(self, device_name: str, status: str, timeout: float = 20.):
+        print("send frame")
+        self._sdp_interface.send(
+            device_name,
+            DataFrame(
+                packet_description="Light control",
+                content=[status],
+            ),
+        )
+        self._sdp_interface.send(
+            device_name,
+            DataFrame(
+                packet_description="Light control",
+                content=[status],
+            ),
+        )
+        self._sdp_interface.send(
+            device_name,
+            DataFrame(
+                packet_description="Light control",
+                content=[status],
+            ),
+        )
+        print("sent frame")
+        deadline = rospy.Time.now() + rospy.Duration(timeout)
+        while not rospy.is_shutdown() and rospy.Time.now() < deadline:
+            rospy.sleep(1.0)
+            try:
+                print(f"Waiting for status updated: {self._light_status_table[device_name]}")
+                if self._light_status_table[device_name] == status:
+                    break
+            except KeyError:
+                pass
+
+    def no_people_behind(self):
+        odom_to_base = self.odom_to_base
+        return (
+            len(
+                [
+                    odom_to_base.Inverse() * person_frame
+                    for person_frame in self.odom_to_people
+                    if (odom_to_base.Inverse() * person_frame).p[0] < 0
+                ]
+            )
+            == 0
+        )
+
+    def no_people_around(self):
+        return len(self.odom_to_people) == 0
+
+    def stop_demo(self):
+        self._running_demo_thread = False
+        self._thread_demo.join()
+
+    def demo(self):
+        #self.is_out = True
+        self.is_out = {}
+        rospy.logwarn('Demo Started')
+        while not rospy.is_shutdown() and self._running_demo_thread:
+            rospy.sleep(1)
+            with self._light_status_table_lock:
+                rospy.loginfo("==========================")
+                rospy.loginfo(f"is_out: {self.is_out}")
+                for device_name, status in self._light_status_table.items():
+                    device_interfaces = self.device_interfaces
+                    for addr, dev_if in device_interfaces.items():
+                        if dev_if["device_name"] == device_name:
+                            if device_name not in self.is_out:
+                                self.is_out[device_name] = True
+                            try:
+                                distance = self._sdp_interface.device_interfaces[addr]["distance"]
+                            except KeyError:
+                                distance = None
+                            rospy.loginfo( "  =======================")
+                            rospy.loginfo(f"  device_name: {device_name}")
+                            rospy.loginfo(f"  distance: {distance}")
+                            rospy.loginfo(f"  status: {status}")
+                            rospy.loginfo(f"  is_out: {self.is_out[device_name]}")
+                            rospy.loginfo( "  =======================")
+                            debug_message = yaml.dump(
+                                {
+                                    "device_name": device_name,
+                                    "distance": distance,
+                                    "status": status,
+                                    "is_out": self.is_out[device_name],
+                                }
+                                )
+                            self._pub_demo_debug.publish(
+                                    String(data=debug_message))
+                            if distance is None:
+                                rospy.loginfo("Distance is none")
+                                continue
+                            if distance < self._turn_on_distance_threshold and self.is_out[device_name]:
+                                self.is_out[device_name] = False
+                                rospy.logwarn(f"is_out[{device_name}] -> False")
+                                if not status:
+                                    rospy.logwarn(f"turn on to {device_name}")
+                                    self.turn_light(device_name, True)
+                            elif distance >= self._turn_on_distance_threshold and not self.is_out[device_name]:
+                                self.is_out[device_name] = True
+                                rospy.loginfo(f"is_out[{device_name}] -> True")
+                                if status:
+                                    pass
+                                    #rospy.logwarn(f"turn off to {device_name}")
+                                    #self.turn_light(device_name, False)
+                            else:
+                                rospy.loginfo("skipped")
+                rospy.loginfo("==========================")
+        rospy.logwarn('Demo stopped')
+
+    def walk(self):
+        default_7f_walk_path = '/home/spot/default_7f_with_door.walk'
+        target_id_73b1 = 'chief-iguana-rCeWvuq9uAAhdq7D+trwMw=='
+        target_id_73b2 = 'yonder-adder-cjebDHNMdwNaax8EdVqs0A=='
+        target_id_73a4 = 'soured-cocoon-KRBT4IqkmBwCauxpgDhFEQ=='
+        start_id = 'holy-puffin-dfM.pGS6xCB4m190VUNPWw=='
+        goal_id = 'unsold-shrew-.KEEBLDLzp+zG8MLyAWU.Q=='
+
+        self._client.upload_graph(default_7f_walk_path)
+        success, message = self._client.set_localization_fiducial()
+        if not success:
+            rospy.logerror("Failed to localize. Aborting.")
+            return
+
+        self._client.navigate_to(start_id, blocking=True)
+        self._reset_localize(TriggerRequest())
+
+        rospy.logwarn('Start')
+        success, message = self._client.navigate_to(target_id_73b1, velocity_limit=self._vel_limit, blocking=True)
+
+        # 73B1
+        if success:
+            time.sleep(10.)
+            no_people_around = self.no_people_around()
+            if not no_people_around:
+                rospy.logwarn("There is people in the room. So I will warn them and leave")
+                self._sound_client.say("Please make sure to lock all doors and windows before leaving the house")
+            else:
+                rospy.logwarn("No people around. So turn off the light")
+                self.turn_light("SDP Switchbot 73B1", False)
+            self._client.navigate_to(target_id_73b2, velocity_limit=self._vel_limit, blocking=False)
+        else:
+            rospy.logwarn("Failed to reach 73B1")
+            self._client.navigate_to(target_id_73b2, velocity_limit=self._vel_limit, blocking=False)
+        self._client.wait_for_navigate_to_result()
+        success, message = self._client.get_navigate_to_result()
+
+
+        # 73B2
+        if success:
+            time.sleep(10.)
+            no_people_around = self.no_people_around()
+            if not no_people_around:
+                rospy.logwarn("There is people in the room. So I will warn them and leave")
+                self._sound_client.say("Please make sure to lock all doors and windows before leaving the house")
+            else:
+                rospy.logwarn("No people around. So turn off the light")
+                self.turn_light("SDP Switchbot", False)
+            self._client.navigate_to(target_id_73a4, velocity_limit=self._vel_limit, blocking=False)
+        else:
+            rospy.logwarn("Failed to reach 73B2")
+            self._client.navigate_to(target_id_73a4, velocity_limit=self._vel_limit, blocking=False)
+        self._client.wait_for_navigate_to_result()
+        success, message = self._client.get_navigate_to_result()
+
+        # 73A4
+        if success:
+            time.sleep(10.)
+            no_people_around = self.no_people_around()
+            if not no_people_around:
+                rospy.logwarn("There is people in the room. So I will warn them and leave")
+                self._sound_client.say("Please make sure to lock all doors and windows before leaving the house")
+            else:
+                rospy.logwarn("No people around. So turn off the light")
+                self.turn_light("SDP Switchbot 73A4", False)
+            self._client.navigate_to(start_id, velocity_limit=self._vel_limit, blocking=False)
+        else:
+            rospy.logwarn("Failed to reach 73A4")
+            self._client.navigate_to(start_id, velocity_limit=self._vel_limit, blocking=False)
+        self._client.wait_for_navigate_to_result()
+        success, message = self._client.get_navigate_to_result()
+
+        self._client.navigate_to(goal_id, blocking=True)
+        rospy.logwarn('End')
+
+
+if __name__ == "__main__":
+    rospy.init_node("light_room_demo")
+    do_walk = rospy.get_param('~do_walk', True)
+    node = LightRoomDemo()
+    if do_walk:
+        node.walk()
+    else:
+        rospy.spin()
+    node.stop_demo()
---
+++ jsk_spot_autonomous_integration_demo/python/autonomous_integration/sdp_utils.py
@@ -0,0 +1,86 @@
+from typing import List, Tuple
+from urllib import response
+
+from smart_device_protocol.smart_device_protocol_interface import (
+    DataFrame,
+    UWBSDPInterface,
+)
+from . import (
+    ARGUMENT_NAMES_AND_TYPES,
+    RESPONSE_NAMES_AND_TYPES,
+)
+
+
+def get_api_list(
+    interface: UWBSDPInterface,
+) -> List[Tuple[str, str, str, ARGUMENT_NAMES_AND_TYPES, RESPONSE_NAMES_AND_TYPES]]:
+    """
+        Convert the list of device information to the list of API information.
+
+    Returns:
+        List[Tuple[str, str, ARGUMENT_NAMES_AND_TYPES, RESPONSE_NAMES_AND_TYPES]]: List of API information.
+            Device name, pub or sub, description, argument names and types, response names and types.
+
+        In [5]: interface.device_interfaces
+    Out[5]:
+    {(36,
+      10,
+      196,
+      248,
+      180,
+      124): {'broadcast_interfaces': [('Key status', '?'),
+       ('UWB Station',
+        'i')], 'device_name': 'SDP Lock 73B2', 'distance': 76.1500015258789, 'interfaces': [('Key control',
+        's')], 'last_stamp': rospy.Time[1677762647464644670], 'uwb_id': 1},
+     (120, 33, 132, 149, 125, 148): {'device_name': 'SDP_DEBUG_BOARD',
+      'distance': None,
+      'interfaces': [],
+      'last_stamp': rospy.Time[1677762647395696878],
+      'uwb_id': None},
+     (120, 33, 132, 149, 126, 152): {'device_name': 'msg_board',
+      'distance': None,
+      'interfaces': [('Message Board message', 'siS')],
+      'last_stamp': rospy.Time[1677762647411919116],
+      'uwb_id': None},
+     (120,
+      33,
+      132,
+      168,
+      0,
+      196): {'broadcast_interfaces': [('Light status', '?'),
+       ('UWB Station',
+        'i')], 'device_name': 'SDP Switchbot', 'distance': 58.34000015258789, 'interfaces': [('Light control',
+        '?')], 'last_stamp': rospy.Time[1677762647317466974], 'uwb_id': 4}}
+    """
+    api_list = []
+    for addr, dev_inf in interface.device_interfaces.items():
+        device_name = dev_inf["device_name"]
+        for interface in dev_inf["interfaces"]:
+            arguments_name_and_types = []
+            for i, arg_type_char in enumerate(interface[1)]:
+                if arg_type_char == "s" or arg_type_char == "S":
+                    arguments_name_and_types.append((f"arg{i}", "string"))
+                elif arg_type_char == "i":
+                    arguments_name_and_types.append((f"arg{i}","int"))
+                elif arg_type_char == "f":
+                    arguments_name_and_types.append((f"arg{i}","float"))
+                elif arg_type_char == "?" or arg_type_char == "b":
+                    arguments_name_and_types.append((f"arg{i}","bool"))
+                else:
+                    raise ValueError(f"Unknown argument type: {arg_type_char}")
+            api_list.append((device_name, "pub", interface[0], arguments_name_and_types, None))
+        for interface in dev_inf["broadcast_interfaces"]:
+            response_name_and_types = []
+            for i, res_type_char in enumerate(interface[1]):
+                if res_type_char == "s" or res_type_char == "S":
+                    response_name_and_types.append((f"res{i}", "string"))
+                elif res_type_char == "i":
+                    response_name_and_types.append((f"res{i}","int"))
+                elif res_type_char == "f":
+                    response_name_and_types.append((f"res{i}","float"))
+                elif res_type_char == "?" or res_type_char == "b":
+                    response_name_and_types.append((f"res{i}","bool"))
+                else:
+                    raise ValueError(f"Unknown response type: {res_type_char}")
+            api_list.append((device_name, "sub", interface[0], None, response_name_and_types))
+    return api_list
