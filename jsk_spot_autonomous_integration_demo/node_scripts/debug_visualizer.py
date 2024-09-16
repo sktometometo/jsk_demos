91e1424214661780b82a12d46cb109664d807804
Koki Shinjo <sktometometo@gmail.com>
2024-09-16 17:47:40 +0900

    update

--- jsk_spot_autonomous_integration_demo/node_scripts/debug_visualizer.py
+++ jsk_spot_autonomous_integration_demo/node_scripts/debug_visualizer.py
@@ -13,7 +13,7 @@ class DebugVisualizer:
 
     def __init__(self):
 
-        self._sub = rospy.Subscriber("/debug_text", String, self._callback)
+        self._sub = rospy.Subscriber("/debug_string", String, self._callback)
         self._pub_api_list = rospy.Publisher(
             "/debug_text_overlay/api_list", OverlayText, queue_size=1
         )
@@ -22,6 +22,16 @@ class DebugVisualizer:
         )
         self._pub_api_call = rospy.Publisher(
             "/debug_text_overlay/api_call", OverlayText, queue_size=1
+        )
+
+        self.timeout_api_list = rospy.Duration(
+            float(rospy.get_param("~timeout_api_list", "10.0"))
+        )
+        self.timeout_target_api = rospy.Duration(
+            float(rospy.get_param("~timeout_target_api", "10.0"))
+        )
+        self.timeout_api_call = rospy.Duration(
+            float(rospy.get_param("~timeout_api_call", "10.0"))
         )
 
         self._api_list: List[Tuple[str, str, List[str]]] = []
@@ -40,6 +50,7 @@ class DebugVisualizer:
         data = yaml.load(msg.data)
         if data["string_type"] == "api_full_list":
             api_list = data["data"]
+            print(f"api_list: {api_list}")
             with self._api_list_lock:
                 self._api_list = [
                     (
@@ -77,6 +88,8 @@ class DebugVisualizer:
     def _publish_api_call(self):
 
         with self._api_call_lock:
+            if rospy.Time.now() - self._api_call_stamp > self.timeout_api_call:
+                return
             if self._api_call is None:
                 return
             api_device_name, api_description, arguments = self._api_call
@@ -88,7 +101,9 @@ class DebugVisualizer:
 
     def _publish_api_list(self):
 
-        with self._api_list_lock and self._target_api_lock:
+        with self._api_list_lock:
+            if rospy.Time.now() - self._api_list_stamp > self.timeout_api_list:
+                return
             text = OverlayText()
             text.text = "API list\n"
             for device_name, description, argument_names_and_types in self._api_list:
@@ -96,5 +111,29 @@ class DebugVisualizer:
                 for name in argument_names_and_types:
                     text.text += f"  {name}\n"
             self._pub_api_list.publish(text)
-        
-    
+
+    def _publish_target_api(self):
+
+        with self._target_api_lock:
+            if rospy.Time.now() - self._target_api_stamp > self.timeout_target_api:
+                return
+            text = OverlayText()
+            text.text = f"Target API\nIntension: {self._target_api_intension}\n"
+            for device_name, description in self._target_api:
+                text.text += f"{device_name}: {description}\n"
+            self._pub_target_api.publish(text)
+
+    def run(self):
+
+        rate = rospy.Rate(1)
+        while not rospy.is_shutdown():
+            self._publish_api_list()
+            self._publish_target_api()
+            self._publish_api_call()
+            rate.sleep()
+
+
+if __name__ == "__main__":
+    rospy.init_node("debug_visualizer")
+    visualizer = DebugVisualizer()
+    visualizer.run()
