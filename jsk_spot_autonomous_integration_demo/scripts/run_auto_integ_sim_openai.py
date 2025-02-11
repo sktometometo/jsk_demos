94d68c19982f74a96a65d0612fc9dff8c2b48bb8
Koki Shinjo <sktometometo@gmail.com>
2025-02-11 14:00:47 +0900

    Update

--- jsk_spot_autonomous_integration_demo/scripts/run_auto_integ_sim_openai.py
+++ jsk_spot_autonomous_integration_demo/scripts/run_auto_integ_sim_openai.py
@@ -10,7 +10,7 @@ from autonomous_integration_simulation.env import *
 from autonomous_integration_simulation.openai_tool_calling import call_device_openai
 
 
-def main(param_file: Optional[str] = None):
+def main(param_file: Optional[str] = None, openai_key: str = ""):
     rospy.init_node("demo")
 
     package_path = rospkg.RosPack().get_path("jsk_spot_autonomous_integration_demo")
@@ -31,13 +31,14 @@ def main(param_file: Optional[str] = None):
         )
         print(environment)
         print(condition)
-        result = call_device(environment, condition[2])
-        print(result)
+        result = call_device_openai(environment, condition[2], openai_key)
+        # print(result)
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("--param_file", type=str)
+    parser.add_argument("--openai_key", type=str)
     args = parser.parse_args()
 
-    main(args.param_file)
+    main(args.param_file, args.openai_key)
